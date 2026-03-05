42f03383dac8de6b634a4323f27900b68515dadd
jhoan sebastian buitrago sandoval <buitragosandovalj964@gmai.com>
2026-03-05 15:49:43 -0500

    feat(main.py):se agrego elmenu en main

---
+++ main.py
@@ -0,0 +1,45 @@
+from datos import cargar_empanadas,guardar_empandas
+from sebastian import editar_empanada, eliminar_empanada
+
+
+def menu():
+
+    empanadas = cargar_empanadas()
+
+    while True:
+
+        print("\n===== MENÚ EMPANADAS DOÑA PEPA =====")
+        print("1. Listar empanadas")
+        print("2. Agregar empanada")
+        print("3. Editar empanada")
+        print("4. Eliminar empanada")
+        print("5. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            listar_empanadas(empanadas)
+
+        elif opcion == "2":
+            agregar_empanada(empanadas)
+            guardar_empanadas(empanadas)
+
+        elif opcion == "3":
+            editar_empanada(empanadas)
+            guardar_empanadas(empanadas)
+
+        elif opcion == "4":
+            eliminar_empanada(empanadas)
+            guardar_empanadas(empanadas)
+
+        elif opcion == "5":
+            guardar_empanadas(empanadas)
+            print("Saliendo del programa...")
+            break
+
+        else:
+            print("Opción inválida")
+
+
+if __name__ == "__main__":
+    menu()
